b3f3268e5bdbb97f974c296820e251f23e0b2385
Rishi Jha <rishijha568@gmail.com>
2025-08-31 23:49:26 +0530

    backend link updated

--- FrontEnd/src/Components/backendLink.js
+++ FrontEnd/src/Components/backendLink.js
@@ -3,5 +3,5 @@
 // "https://linkbee.onrender.com";
 
 
-const backendLink = "https://linkbee-0240.onrender.com"
+const backendLink = "https://linkbee-0240.onrender.com";
 export default backendLink; 
