8aa5a9f0932c17ad50d99f1763b747ce22d5f7a8
Rishi Jha <rishijha568@gmail.com>
2025-09-01 00:57:53 +0530

    profile link added

--- FrontEnd/src/Components/backendLink.js
+++ FrontEnd/src/Components/backendLink.js
@@ -1,6 +1,5 @@
 // const backendLink = "http://localhost:3000";
 // const backendLink = "https://linkbeebackend.vercel.app";
-// "https://linkbee.onrender.com";
 
 
 const backendLink = "https://linkbee-kn2i.onrender.com";
